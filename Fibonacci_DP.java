81a975bde130d69ff4b72148053f68139f65559d
Gaganjot Kaur Kang <gkang050@gmail.com>
2017-01-18 20:15:15 -0800

    dp commit

---
+++ Fibonacci_DP.java
@@ -0,0 +1,9 @@
+public int fibDP(int x) {
+		int fib[] = new int[x + 1];
+		fib[0] = 0;
+		fib[1] = 1;
+		for (int i = 2; i < x + 1; i++) {
+			fib[i] = fib[i - 1] + fib[i - 2];
+		}
+		return fib[x];
+	}
